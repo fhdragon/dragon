d2624dc92d06836530669e387dcdf3961b30dccd
Dragon <wenlong.meng@gmail.com>
2013-06-03 14:31:39 +0800

    add Filter

---
+++ dragon-commons/src/main/java/org/apache/dragon/commons/lang/Filter.java
@@ -0,0 +1,33 @@
+package org.apache.dragon.commons.lang;
+
+/**
+ * StringFilter: String filter interface
+ * 
+ * @author Wenlong Meng(wenlong.meng@gmail.com)
+ * @version 1.0 at 2012/04/27
+ * @since 1.0
+ */
+public interface Filter<T> {
+
+	// Logic
+	/**
+	 * encode the specified t
+	 * 
+	 * @param t
+	 *            The t to be filtered
+	 * @return null - if t is null
+	 * @throws NullPointerException
+	 *             - if t is null
+	 */
+	T encode(T t);
+
+	/**
+	 * decode the specified t
+	 * 
+	 * @param t
+	 * @throws NullPointerException
+	 *             if t is null
+	 */
+	T decode(T t);
+
+}
